0322c881dde9b571a9dd63873f9bb18d54ddbc0c
xmondo <xmondo@xmondo.com>
2023-12-15 14:49:40 -0600

    Create trig_plot.py

---
+++ trig_plot.py
@@ -0,0 +1,22 @@
+import chaos_plot_classes as cp
+import numpy as np
+
+# max number of frames is 350
+
+number_of_frames = 200
+frames = np.linspace(0, 1, number_of_frames)
+image_frame_number = 1
+low_res = 500
+medium_res = 1000
+high_res = 10000
+very_high_res = 100000
+plot_dir = '/home/xmondo/Desktop/plots'
+try:
+    print('number of frames: ', len(frames))
+    for k in frames:
+        print(k)
+        plot = cp.ChaosTrigPlot(8, 12, 2, -1, 1, high_res, k, image_frame_number, plot_dir)
+        plot.fplot()
+        image_frame_number += 1
+except Exception as err:
+    print('Exception', err)
